1324f00f7447dde358c8be79dec2039f8c24d4cf
Tyler King <tyler.king@newfie.co>
2013-12-31 16:01:27 -0330

    Working gEdit 3

--- duplicate.py
+++ duplicate.py
@@ -1,89 +1,94 @@
 from gettext import gettext as _
-import gtk
-import gedit
+from gi.repository import GObject, Gtk, Gedit
 
-ui_str = """
-<ui>
-	<menubar name="MenuBar">
-		<menu name="ToolsMenu" action="Tools">
-			<placeholder name="Tools_Ops2">
-				<menuitem name="Duplicate" action="Duplicate" />
-			</placeholder>
-		</menu>
-	</menubar>
+# Place the menu item above "Select All" in "Edit".
+ui_str = """<ui>
+  <menubar name="MenuBar">
+    <menu name="EditMenu" action="Edit">
+      <placeholder name="EditOps_2">
+        <menuitem name="DuplicateLine" action="DuplicateLine"/>
+      </placeholder>
+    </menu>
+  </menubar>
 </ui>
 """
 
-class DuplicateWindowHelper:
-	def __init__(self, plugin, window):
-		self._window = window
-		self._plugin = plugin
+class DuplicateLineWindowActivatable(GObject.Object, Gedit.WindowActivatable):
+    __gtype_name__ = "DuplicateLineWindowActivatable"
 
-		self._insert_menu()
-		
-	def deactivate(self):
-		self._remove_menu()
-		self._window = None
-		self._plugin = None
-		self._action_group = None
+    window = GObject.property(type=Gedit.Window)
 
-	def _insert_menu(self):
-		manager = self._window.get_ui_manager()
-		self._action_group = gtk.ActionGroup("DuplicatePluginActions")
-		self._action_group.add_actions([("Duplicate", None, _("Duplicate"), "<control><shift>d", _("Duplicate current line, current selection or selected lines"), self.on_duplicate_activated)])
-		manager.insert_action_group(self._action_group, -1)
-		self._ui_id = manager.add_ui_from_string(ui_str)
+    def __init__(self):
+        GObject.Object.__init__(self)
 
-	def _remove_menu(self):
-		manager = self._window.get_ui_manager()
-		manager.remove_ui(self._ui_id)
-		manager.remove_action_group(self._action_group)
-		manager.ensure_update()
-	
-	def update_ui(self):
-		pass
+    def do_activate(self):
+        self._insert_menu()
 
-	def on_duplicate_activated(self, action):
-		doc = self._window.get_active_document()
+    def do_deactivate(self):
+        self._remove_menu()
 
-		if doc.get_has_selection():
-			s, e = doc.get_selection_bounds()
-			l1 = s.get_line()
-			l2 = e.get_line()
+        self._action_group = None
 
-			if l1 != l2:
-				s.set_line_offset(0)
-				e.forward_to_line_end()
-				text = "\n" + doc.get_text(s, e)
-				doc.insert(e, text)
-			else:
-				text = doc.get_text(s, e)
-				doc.move_mark_by_name("selection_bound", s)
-				doc.insert(e, text)
-				
-		else:
-			s = doc.get_iter_at_mark(doc.get_insert())
-			e = doc.get_iter_at_mark(doc.get_insert())
-			s.set_line_offset(0)
-			if not e.ends_line():
-				e.forward_to_line_end()
-			text = "\n" + doc.get_text(s, e)
-			doc.insert(e, text)
+    def _insert_menu(self):
+        manager = self.window.get_ui_manager()
 
+        # Add our menu action and set ctrl+shift+D to activate.
+        self._action_group = Gtk.ActionGroup("DuplicateLinePluginActions")
+        self._action_group.add_actions([(
+            "DuplicateLine",
+            None,
+            _("Duplicate Line"),
+            "<control><shift>d",
+            _("Duplicate current line, current selection or selected lines"),
+            self.on_duplicate_line_activate
+        )])
 
-class DuplicatePlugin (gedit.Plugin):
-	def __init__(self):
-		gedit.Plugin.__init__(self)
-		self._instances = {}
-		
-	def activate(self, window):
-		self._instances[window] = DuplicateWindowHelper(self, window)
-		
-	def deactivate(self, window):
-		self._instances[window].deactivate()
-		del self._instances[window]	
-		
-	def update_ui(self, window):
-		self._instances[window].update_ui()
+        manager.insert_action_group(self._action_group, -1)
 
+        self._ui_id = manager.add_ui_from_string(ui_str)
 
+    def _remove_menu(self):
+        manager = self.window.get_ui_manager()
+        manager.remove_ui(self._ui_id)
+        manager.remove_action_group(self._action_group)
+        manager.ensure_update()
+
+    def do_update_state(self):
+        self._action_group.set_sensitive(self.window.get_active_document() != None)
+
+    def on_duplicate_line_activate(self, action):
+        doc = self.window.get_active_document()
+        if not doc:
+            return
+
+        if doc.get_has_selection():
+            # User has text selected, get bounds.
+            s, e = doc.get_selection_bounds()
+            l1   = s.get_line()
+            l2   = e.get_line()
+
+            if l1 != l2:
+                # Multi-lines selected. Grab the text, insert.
+                s.set_line_offset(0)
+                e.forward_to_line_end()
+
+                text = "\n" + doc.get_text(s, e, False)
+
+                doc.insert(e, text)
+            else:
+                # Same line selected. Grab the text, insert on same line after selection.
+                text = doc.get_text(s, e, False)
+                doc.move_mark_by_name("selection_bound", s)
+                doc.insert(e, text)
+        else:
+            # No selection made. Grab the current line the cursor is on, insert on new line.
+            s = doc.get_iter_at_mark(doc.get_insert())
+            e = doc.get_iter_at_mark(doc.get_insert())
+            s.set_line_offset(0)
+
+            if not e.ends_line():
+                e.forward_to_line_end()
+
+            text = "\n" + doc.get_text(s, e, False)
+
+            doc.insert(e, text)
